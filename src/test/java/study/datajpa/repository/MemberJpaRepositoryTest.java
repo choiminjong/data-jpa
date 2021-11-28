0f96f57bdb612362bf82a592767bced66f550a34
alswhd20 <alswhd20@gmail.com>
2021-11-28 17:59:43 +0900

    벌크성 수정 쿼리

--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -120,4 +120,17 @@ class MemberJpaRepositoryTest {
         Assertions.assertThat(totalCount).isEqualTo(5);
     }
 
+    @Test
+    public void bulkUpdata(){
+        //given
+        memberJpaRepository.save(new Member("member1",10));
+        memberJpaRepository.save(new Member("member2",19));
+        memberJpaRepository.save(new Member("member3",20));
+        memberJpaRepository.save(new Member("member4",21));
+        memberJpaRepository.save(new Member("member5",40));
+
+        //when
+        int resultCount = memberJpaRepository.bulkAgePlus(20);
+        Assertions.assertThat(resultCount).isEqualTo(3);
+    }
 }
